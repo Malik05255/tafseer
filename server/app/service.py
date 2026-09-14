8460bf3d1b8f0936209699ed5e0c28b80bb340d7
Malik05255 <mmbbrr99@gmail.com>
2026-09-14 23:58:32 +0300

    Activate Quran and Sunnah grounded tafseer service

--- server/app/service.py
+++ server/app/service.py
@@ -1,3 +1,4 @@
-from .compact_service import AnalysisTechnicalFailure, TafseerService
+from .compact_service import AnalysisTechnicalFailure
+from .scripture_service import TafseerService
 
 __all__ = ["AnalysisTechnicalFailure", "TafseerService"]
